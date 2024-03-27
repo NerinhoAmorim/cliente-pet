62096242668dfc5b86e7a343b54158c3864ceff4
Nerinho Nascimento <neramornas@gmail.com>
2024-03-27 15:03:07 -0300

    implementa API  de teste

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -2,9 +2,19 @@ package br.com.petz.clientepet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetApplication {
+	@GetMapping
+	public String getHomeTeste(){
+		return "Cliente Pet _ API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetApplication.class, args);
