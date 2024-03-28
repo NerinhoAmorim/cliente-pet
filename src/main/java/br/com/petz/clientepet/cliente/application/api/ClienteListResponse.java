a1e9a72a20575adc39abcea7affb6c6e9cb21086
Nerinho Nascimento <neramornas@gmail.com>
2024-03-28 15:02:24 -0300

    implementa fluxo api, teste ok!

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,11 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import java.util.UUID;
+
+public class ClienteListResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String celular;
+	private String email;
+}
