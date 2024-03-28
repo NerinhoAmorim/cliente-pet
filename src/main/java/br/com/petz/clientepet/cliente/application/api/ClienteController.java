8953900f177ec149a6a5b9dec8b51083e2600d42
Nerinho Nascimento <neramornas@gmail.com>
2024-03-28 00:35:24 -0300

    implementa controller e fluxo para service

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -2,17 +2,24 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteController implements ClienteAPI {
+
+	private final ClienteService clienteService;
 
 	@Override
 	public ClienteResponse postCliente(ClienteRequest clienteRequest) {
-		log.info("[inicia] ClienteController - postCliente" );
-		log.info("[finaliza] ClienteController - postCliente" );
-		return null;
+		log.info("[inicia] ClienteController - postCliente");
+		ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
+		log.info("[finaliza] ClienteController - postCliente");
+		return clienteCriado;
 	}
 
 }
