6f90d588416a251d376b3117826720173f4b6132
Nerinho Nascimento <neramornas@gmail.com>
2024-03-28 01:58:00 -0300

    implementa ClienteApplicationService e novo construtor Cliente

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package br.com.petz.clientepet.cliente.application.repository;
+
+import br.com.petz.clientepet.cliente.domain.Cliente;
+
+public interface ClienteRepository {
+
+	Cliente salva(Cliente cliente);
+	
+}
