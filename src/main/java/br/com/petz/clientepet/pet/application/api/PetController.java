d812dc3a12957dc51139b7c79dd3b627604bf567
Nerinho Nascimento <neramornas@gmail.com>
2024-04-03 12:54:17 -0300

    implementa fluxo service

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.petz.clientepet.pet.application.service.PetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 @RestController
@@ -13,12 +14,15 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class PetController implements PetAPI {
 
+	private final PetService petService;
+
 	@Override
 	public PetResponse postPet(UUID idCliente, @Valid PetRequest petRequest) {
 		log.info("[inicia] PetController - postPet ");
 		log.info("[idCliente] {}", idCliente );
+		PetResponse pet = petService.criaPet(idCliente, petRequest);
 		log.info("[finaliza] PetController - postPet ");
-		return null;
+		return pet;
 	}
 
 }
