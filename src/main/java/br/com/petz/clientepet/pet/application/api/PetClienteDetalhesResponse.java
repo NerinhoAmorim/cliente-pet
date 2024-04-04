e72e38208d90f7803c964a50627dd4ba39008e7e
Nerinho Nascimento <neramornas@gmail.com>
2024-04-04 01:03:25 -0300

    implementa fluxo da camada de API

---
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetClienteDetalhesResponse.java
@@ -0,0 +1,23 @@
+package br.com.petz.clientepet.pet.application.api;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import br.com.petz.clientepet.pet.domain.Porte;
+import br.com.petz.clientepet.pet.domain.SexoPet;
+import br.com.petz.clientepet.pet.domain.TipoPet;
+import lombok.Value;
+@Value
+public class PetClienteDetalhesResponse {
+	private UUID idPet;
+	private String nomePet;
+	private Porte porte;
+	private TipoPet tipoPet;
+	private String microchip;
+	private String raca;
+	private SexoPet sexo;
+	private String pelagemCor;
+	private LocalDate dataNascimento;
+	private String rga;
+	private Integer peso;
+}
